34de4959d08d2484841705d4bbb39687a7e8b0a6
Navaneeth1510 <1rn21cs098.navaneeth@gmail.com>
2025-02-10 13:26:15 +0530

    The errors are handled

--- Calculator.java
+++ Calculator.java
@@ -4,9 +4,13 @@ import java.util.Stack;
 public class Calculator {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        String exp = sc.next();
-        System.out.println("The result of the expression is : "+evaluateExpression(exp));
+        while (true) {
+            System.out.println("\nPlease enter the expression : ");
+            String exp = sc.next();
+            System.out.println("The result of the expression is : " + evaluateExpression(exp));
+        }
     }
+
     public static int precedence(char op) {
         return (op == '+' || op == '-') ? 1 : (op == '*' || op == '/') ? 2 : 0;
     }
@@ -16,7 +20,12 @@ public class Calculator {
             case '+' -> a + b;
             case '-' -> a - b;
             case '*' -> a * b;
-            case '/' -> a / b;
+            case '/' -> {
+                if(b==0) {
+                    throw new ArithmeticException("Denominator is 0");
+                }
+                yield a / b;
+            }
             default -> 0;
         };
     }
@@ -25,13 +34,15 @@ public class Calculator {
         Stack<Double> values = new Stack<>();
         Stack<Character> ops = new Stack<>();
 
-        int i=0;
-        while(i<expression.length()){
+        int i = 0;
+        while (i < expression.length()) {
             char ch = expression.charAt(i);
-            if(ch==' '){
+
+            if (ch == ' ') {
                 i++;
                 continue;
             }
+
             if (Character.isDigit(ch)) {
                 StringBuilder num = new StringBuilder();
                 while (i < expression.length() && (Character.isDigit(expression.charAt(i)) || expression.charAt(i) == '.')) {
@@ -41,30 +52,32 @@ public class Calculator {
                 continue;
             }
 
-            if(ch=='('){
+            if (ch == '(') {
                 ops.push(ch);
-            }
-            else if (ch == ')') {
+            } else if (ch == ')') {
                 while (!ops.isEmpty() && ops.peek() != '(') {
-                    values.push(calculate(values.pop(), values.pop(), ops.pop()));
+                    double b = values.pop();
+                    double a = values.pop();
+                    values.push(calculate(a, b, ops.pop()));
                 }
                 ops.pop();
-            }
-            else if (ch == '+' || ch == '-' || ch == '*' || ch == '/') {
+            } else if (ch == '+' || ch == '-' || ch == '*' || ch == '/') {
                 while (!ops.isEmpty() && precedence(ops.peek()) >= precedence(ch)) {
-                    values.push(calculate(values.pop(), values.pop(), ops.pop()));
+                    double b = values.pop();
+                    double a = values.pop();
+                    values.push(calculate(a, b, ops.pop()));
                 }
                 ops.push(ch);
             }
-
             i++;
         }
 
-        while(!ops.isEmpty()){
-            values.push(calculate(values.pop(), values.pop(), ops.pop()));
+        while (!ops.isEmpty()) {
+            double b = values.pop();
+            double a = values.pop();
+            values.push(calculate(a, b, ops.pop()));
         }
 
         return values.pop();
     }
-
 }
