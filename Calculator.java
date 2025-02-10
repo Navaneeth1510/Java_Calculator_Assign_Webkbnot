b63f2df2aacca81777cec8fb97c04f896dae5754
Navaneeth1510 <1rn21cs098.navaneeth@gmail.com>
2025-02-10 12:37:49 +0530

    Precedance added

--- Calculator.java
+++ Calculator.java
@@ -6,6 +6,10 @@ public class Calculator {
         String exp = sc.next();
         System.out.println("The result of the expression is : "+evaluateExpression(exp));
     }
+    public static int precedence(char op) {
+        return (op == '+' || op == '-') ? 1 : (op == '*' || op == '/') ? 2 : 0;
+    }
+
     public static double evaluateExpression(String expression) {
         System.out.println(expression);
         return 0.0;
